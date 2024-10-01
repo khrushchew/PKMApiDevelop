5e7b0e21338c94228b213974277a7dcec826281c
Никита <popovn2268@gmail.com>
2024-10-01 23:41:45 +0300

    upd

--- PKMApi/PlanovikApi/serializers.py
+++ PKMApi/PlanovikApi/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, Batch
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
+        fields = '__all__'
+
+class BatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Batch
         fields = '__all__'
--- PKMApi/PlanovikApi/views.py
+++ PKMApi/PlanovikApi/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, Batch
+from .serializers import OrderSerializer, BatchSerializer
 
 class OrderAPIView(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+class BatchAPIView(generics.ListAPIView):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
