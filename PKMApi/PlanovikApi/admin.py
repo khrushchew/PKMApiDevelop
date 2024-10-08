7774522e3c0c7b98c1120bc6fa5c20ff04693e00
Никита <popovn2268@gmail.com>
2024-10-08 13:29:12 +0300

    Clearing

--- PKMApi/PlanovikApi/admin.py
+++ PKMApi/PlanovikApi/admin.py
@@ -3,10 +3,3 @@ from django.contrib import admin
 from django.apps import apps
 # Register your models here.
 
-models = apps.get_models()
-
-for model in models:
-    try:
-        admin.site.register(model)
-    except admin.exceptions.AlreadyRegistered:
-        pass
