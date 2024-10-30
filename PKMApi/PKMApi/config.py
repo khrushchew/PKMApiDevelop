ca7ff1a3ee6162c592bd8993bbce31d999697971
Никита <popovn2268@gmail.com>
2024-10-30 09:18:49 +0300

    conf

---
+++ PKMApi/PKMApi/config.py
@@ -0,0 +1,21 @@
+SECRET_KEY = "django-insecure-g6%&9mu7^h20+7y9ujzw=kl=qiv)p+@@fol+1^xg9^k!g7yw77"
+
+AWS_ACCESS_KEY_ID = 'YCAJEm-2S-mHduWCtZqItQD1G'
+AWS_SECRET_ACCESS_KEY = 'YCOrn0-JVO8Ale7lyFdEtKyg9ZPuOR-dkDCZEbzF'
+AWS_STORAGE_BUCKET_NAME = 'userstoragepkm'
+AWS_S3_ENDPOINT_URL = 'https://storage.yandexcloud.net'
+AWS_S3_REGION_NAME = 'ru-central1'
+
+DEFAULT_FILE_STORAGE = 'yandex_s3_storage.ClientImgStorage'
+MEDIA_URL = 'https://userstoragepkm.storage.yandexcloud.net/media/'
+
+DATABASES = {
+'default':
+{
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'main_db',
+'USER': 'postgres',
+'PASSWORD': '12345678',
+'HOST': 'localhost',
+'PORT': '5432',},
+}
