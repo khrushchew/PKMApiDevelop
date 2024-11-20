03bc12e473cc96b210fee47567fab5e2dd3f1797
Никита <popovn2268@gmail.com>
2024-11-20 12:06:40 +0300

    upd: fix_about

--- PKMApi/Core/AboutApi/Views/AboutViewSet.py
+++ PKMApi/Core/AboutApi/Views/AboutViewSet.py
@@ -16,26 +16,18 @@ class AboutApiViewSet(ViewSet):
         operation_description="Получает текущую информацию о приложении, включая версию, дату обновления, разработчика и контактные данные.",
         tags=['about'],
         responses={
-            200: openapi.Response(
-                description="Успешный ответ",
-                examples={
-                    "application/json": {
-                        "about application": {
-                            "version": "1.0",
-                            "update": "2024-01-01",
-                            "developer": "ООО ПКМ"
-                        },
-                        "contacts": {
-                            "address": "ул. Пример, д. 1"
-                        },
-                        "info": "Некоторая правовая информация",
-                        "instructions": "Некоторые инструкции"
+            200: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'about application': openapi.Schema(type=openapi.TYPE_STRING, description='О приложении'),
+                    "contacts": openapi.Schema(type=openapi.TYPE_STRING, description='Контакты'),
+                    "info": openapi.Schema(type=openapi.TYPE_STRING, description="Некоторая правовая информация"),
+                    "instructions": openapi.Schema(type=openapi.TYPE_STRING, description="Некоторые инструкции"),
                     }
-                }
-            ),
+                    ),
             404: openapi.Response(description="Неверное значение запроса"),
-        },
-    )
+            }
+        )
     def list(self, request, *args, **kwargs):
         about = About.objects.all()
         serializer = AboutSerializer(about, many=True)
