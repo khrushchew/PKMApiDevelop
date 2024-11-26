78c3416fa9fbf0b3e2e1fc55e818a1635ea166da
khrushchew <popovn2268@gmail.com>
2024-11-26 13:27:45 +0300

    Update settings.py

--- PKMApi/PKMApi/settings.py
+++ PKMApi/PKMApi/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
-from .config import SECRET_KEY, DATABASES
+from .config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
